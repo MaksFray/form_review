f00a998688331ef1f9dfdde524088fcc98d5b570
Maks <fray09@inbox.ru>
2024-06-05 19:17:33 +0600

    Add success page for form feedback app

--- form_review/feedback/views.py
+++ form_review/feedback/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        second_name = request.POST['second_name']
+        feedback = request.POST['feedback']
+        return HttpResponseRedirect('/done')
     return render(request, 'feedback/feedback.html')
+
+def done(request):
+    return render(request, 'feedback/done.html')
