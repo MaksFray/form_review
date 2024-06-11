319283746dc472796323c616ba4ea37d05b3073f
Maks <fray09@inbox.ru>
2024-06-11 19:06:21 +0600

    Add template view for feedback app

--- form_review/feedback/views.py
+++ form_review/feedback/views.py
@@ -22,6 +22,12 @@ class FeedBackView(View):
 class DoneView(TemplateView):
     template_name = 'feedback/done.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Ivan'
+        context['date'] = '20.02.20'
+        return context
+
 class FeedBackUpdateView(View):
     def get(self, request, id_feedback):
         feed = Feedback.objects.get(id=id_feedback)
