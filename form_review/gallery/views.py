c4bc5ec842617187e7283a46adfe1777a137eaaa
Maks <fray09@inbox.ru>
2024-06-17 21:18:38 +0600

    Add load image form to Gallery App

--- form_review/gallery/views.py
+++ form_review/gallery/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
+from .forms import GalleryUploadForm
 # Create your views here.
 
 def storage_file(file):
@@ -9,8 +11,16 @@ def storage_file(file):
 
 class GalleryView(View):
     def get(self, request):
-        return render(request, 'gallery/load_file.html')
+        form = GalleryUploadForm()
+        return render(request, 'gallery/load_file.html', context={
+            'form': form,
+        })
 
     def post(self, request):
-        storage_file(request.FILES['image'])
-        return render(request, 'gallery/load_file.html')
+        form = GalleryUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            storage_file(form.cleaned_data['image'])
+            return HttpResponseRedirect('load_image')
+        return render(request, 'gallery/load_file.html', context={
+            'form': form,
+        })
