2dab8ec12bd3148591627c697d143bd68f32b266
Maks <fray09@inbox.ru>
2024-06-19 23:29:58 +0600

    Add listview for image to Gallery App

--- form_review/gallery/views.py
+++ form_review/gallery/views.py
@@ -2,29 +2,14 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 from .forms import GalleryUploadForm
 from .models import Gallery
 
-
-# Create your views here.
-
-class GalleryView(View):
-    def get(self, request):
-        form = GalleryUploadForm()
-        return render(request, 'gallery/load_file.html', context={
-            'form': form,
-        })
-
-    def post(self, request):
-        form = GalleryUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            new_image = Gallery(image=form.cleaned_data['image'])
-            new_image.save()
-            return HttpResponseRedirect('load_image')
-        return render(request, 'gallery/load_file.html', context={
-            'form': form,
-        })
-
+class ListGallery(ListView):
+    model = Gallery
+    template_name = 'gallery/list_file.html'
+    context_object_name = 'images'
 
 class CreateGalleryView(CreateView):
     model = Gallery
